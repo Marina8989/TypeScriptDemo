3ac6fd97ef80c0f0762ed1cd327761e7242f91fc
Marina8989 <marina.mill209@gmail.com>
2022-01-17 22:27:27 -0800

    ty

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+// const person: {
+//     name: string;
+//     age: number;
+// }
 var person = {
     name: 'Max',
-    age: 30
+    age: 30,
+    hobbies: ['sports', 'cooking']
 };
+var favActiveities;
+favActiveities = ['Sports'];
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
 console.log(person);
--- app.ts
+++ app.ts
@@ -4,7 +4,15 @@
 // }
 const person = {
     name: 'Max',
-    age: 30
+    age: 30,
+    hobbies: ['sports', 'cooking']
+}
+
+let favActiveities: string[];
+favActiveities = ['Sports'];
+
+for(const hobby of person.hobbies) {
+   console.log(hobby.toUpperCase());
 }
 
 console.log(person)
