13c0e5c411d407c1eb1d115146595a123b2320c8
Marina8989 <marina.mill209@gmail.com>
2022-01-17 21:16:27 -0800

    ty

--- app.ts
+++ app.ts
@@ -1,12 +1,9 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
-
-
-function add(num1: number, num2: number){
-    return num1 + num2;
+function add(n1: number, n2: number){
+    return n1 + n2;
 }
 
-button.addEventListener('click', function() {
-    console.log(add(+input1.value, +input2.value))
-})
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result)
